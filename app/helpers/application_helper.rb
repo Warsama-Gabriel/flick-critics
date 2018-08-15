2bf038a12d04a07ba6be246ac02a8f6411d8ce12
Warsama Gabriel <srobleh@gmail.com>
2018-08-14 21:25:40 -0700

    Added active links logic for the application.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,33 @@
 module ApplicationHelper
+
+	def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: all_reviews_path,
+        title: 'Reviews'
+      },
+      {
+        url: all_movies_path,
+        title: 'Movies'
+      }
+    ]
+  end
+
+
+
+  def nav_helper style, tag_type
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+    nav_links.html_safe #interpret the html before rendering it on the screen.
+  end
+
+  def active? path
+    "active" if current_page?(path)
+  end
 end
