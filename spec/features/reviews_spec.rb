54a2ebdabfa69b13a99691dc154608641df88c1b
Warsama Gabriel <srobleh@gmail.com>
2018-08-14 21:15:25 -0700

    added one more feature test.

--- spec/features/reviews_spec.rb
+++ spec/features/reviews_spec.rb
@@ -1,19 +1,18 @@
 require 'rails_helper'
+require 'pry'
 
 RSpec.feature "Reviews", type: :feature do
   scenario "user creates a new review", js: true do 
   	review = FactoryBot.create(:review)
-
-  	visit root_path
+		
+		visit root_path
   	first(:link, "write a review").click
   	sleep 2
   	find("img[alt='1']").click
-  	find("img[alt='2']").click
-  	find("img[alt='3']").click
   	fill_in "Email", with: review.email
   	fill_in 'review_body', with: review.body
   	click_button "Submit"
-  	save_and_open_page
   	expect(page).to have_text review.body
+  	expect(page.has_css?('div.review-data')).to be true
   end
 end
